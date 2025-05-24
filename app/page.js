c32a93f0360ae0602143bc30e0e1e8203ed6a9e1
AdraProductStudio <adraforyou@gmail.com>
2025-05-24 13:44:45 +0530

    UPDATE: ENV consoled in main page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_API_KEY); // hiii
+  console.log(process.env.NEXT_PUBLIC_API_KEY_1); // hello
+  console.log(process.env.NEXT_PUBLIC_API_KEY_2); // success
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
